e6501f6dd0fa7009a921f7e98303883d688c9ada
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2024-03-14 15:44:49 +0530

    Update useGetMessages.js

--- client/src/hooks/useGetMessages.js
+++ client/src/hooks/useGetMessages.js
@@ -10,7 +10,7 @@ const useGetMessages = () => {
 		const getMessages = async () => {
 			setLoading(true);
 			try {
-				const res = await fetch(`http://localhost:5000/api/messages/${selectedConversation._id}`);
+				const res = await fetch(`https://chatapp-1-qtym.onrender.com/api/messages/${selectedConversation._id}`);
 				const data = await res.json();
 				if (data.error) throw new Error(data.error);
 				setMessages(data);
